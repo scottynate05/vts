b3c26466de4f15a1787b7486451f414fd50681b0
Nathan Brown <scottynate05@yahoo.com>
2023-01-25 23:58:01 -0800

    Built out tsconfig.json file and declarations.d.ts file. App ready for Typescript.

---
+++ vts/src/App.tsx
@@ -0,0 +1,42 @@
+// import React from 'react';
+// import { Route } from 'react-router-dom';
+
+// import Projects from './Components/Projects'
+
+import logo from './VerbatimBanner.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <title>Verbatim Studios</title>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          It all starts here and now...
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/scottynate05"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Let's Chat
+        </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/scottydontcode"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Expression
+        </a>
+      </header>
+      {/* <Route path='/projects'>
+        <Projects />
+      </Route> */}
+    </div>
+  );
+}
+
+export default App;
--- vts/src/Components/Projects.tsx
+++ vts/src/Components/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Projects() {
     return(
         <div>
-            
+            <h1>Test</h1>
         </div>
     );
 }
