14413f27d35877540f243762bc11d29217d8d2b6
Nathan Brown <scottynate05@yahoo.com>
2023-01-29 21:09:44 -0800

    Working through some conversions.

--- vts/src/index.js
+++ vts/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// import Projects from './Components/Projects'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <BrowserRouter>
+  //   <Routes>
+  //     <App />
+  //     <Route exact path='/projects' component={Projects} />
+  //   </Routes>
+  // </BrowserRouter>
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
