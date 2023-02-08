bfb79b4a976d97e735f836bd3d5d70bc56db7f27
Nathan Brown <scottynate05@yahoo.com>
2023-02-07 23:34:05 -0800

    Minor tweaks

--- vts/src/Components/Projects.tsx
+++ vts/src/Components/Projects.tsx
@@ -9,6 +9,15 @@ export default function Projects() {
             <li>
                 List of Projects from github.
             </li>
+            <p>
+                Oh, wow. That's an intense line of questioning, Snuffles Hey listen, you know, if we're all bored over here, wouldn't the common denominator be you? What about the reality where Hitler cured cancer, Morty? The answer is: Don't think about it. Your blood will be my lotion!
+
+                Well, to be honest, I'm kind of grossed out with the sexual nature of how everything unfolded. I didn't know how sexual dragons were. I kind of just wanted to do some D&D stuff, y'know? Congratulations on making it into print media. Real bright future there. Nice to wheat you! I wish that shotgun was my penis.
+
+                Yea. If you spend all day shuffling words around you can make anything sound bad, Morty. I don't like it here Morty. I can't abide bureaucracy. I don't like being told where to go and what to do. I consider it a violation. Did you get those seeds all the way up your butt? They'll just fall right out of my ass! I've done this too many times! Nothing to read into there!
+
+                I mean, why would a poptart want to live inside a toaster, Rick? I mean, that would be like the scariest place for them to live. You know what I mean? I know you're real because i have a ton of bad memories with you. Oh I say good sir, oh harumph, ohh a bope-a-dope-o-bobo. I dunno, some people would pay top dollar for that kind of breakthrough.
+            </p>
         </div>
     );
 }
