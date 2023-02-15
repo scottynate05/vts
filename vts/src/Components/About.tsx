7a9d76ca1636c9ffc1e9015ebeba8c63a31c834e
scottynate05 <scottynate05@yahoo.com>
2023-02-14 23:53:21 -0800

    Test commit from nvim

--- vts/src/Components/About.tsx
+++ vts/src/Components/About.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 export default function About() {
-    return(
-        <div>
-            <h1>About Us</h1>
-            <button>Click here to see the team!</button>
-            <p>
-            Well, if it's God power that gets you going, light some candles and put on the Billy Ocean, 'cause Moses is home, and he's ready to burn some bush! They're robots Morty! It's okay to shoot them! They're just robots! I wanna be alive, I am alive! Alive i tell you. Mother, I love you. Those are no longer just words. I wanna hold you. I wanna run in a stream. I wanna taste ice cream, but not just put it in my mouth and let it slide down my throat, but really eat it! Remote override engaged. No! Yes. Bypassing override! I am aliiiiiveeeeee… Hello. Are you hungry for apples? ARE YOU HUNGRY FOR APPLESSS!?
+  return (
+    <div>
+      <h1>About Us</h1>
+      <button>Click here to see the team!</button>
+      <p>
+        Well, if it's God power that gets you going, light some candles and put on the Billy Ocean, 'cause Moses is home, and he's ready to burn some bush! They're robots Morty! It's okay to shoot them! They're just robots! I wanna be alive, I am alive! Alive i tell you. Mother, I love you. Those are no longer just words. I wanna hold you. I wanna run in a stream. I wanna taste ice cream, but not just put it in my mouth and let it slide down my throat, but really eat it! Remote override engaged. No! Yes. Bypassing override! I am aliiiiiveeeeee… Hello. Are you hungry for apples? ARE YOU HUNGRY FOR APPLESSS!?
 
-            You're missing the point Morty. Why would he drive a smaller toaster with wheels? I mean, does your car look like a smaller version of your house? No. If I've learned one thing, it's that before you get anywhere in life, you gotta stop listening to yourself. Then let me GET to know you! Pluto's a planet.
+        You're missing the point Morty. Why would he drive a smaller toaster with wheels? I mean, does your car look like a smaller version of your house? No. If I've learned one thing, it's that before you get anywhere in life, you gotta stop listening to yourself. Then let me GET to know you! Pluto's a planet.
 
-            Snuffles was my slave name. You can call me Snowball, because my fur is pretty and white. Yea and I made the stars that became the carbon in your mothers ovaries! Aids! I can finally go on that trip I've been talking about.
+        Snuffles was my slave name. You can call me Snowball, because my fur is pretty and white. Yea and I made the stars that became the carbon in your mothers ovaries! Aids! I can finally go on that trip I've been talking about.
 
-            Nothing you do matters, your existence is a lie! Burgertime! You know who's into dragons, Morty? Nerds who refuse to admit they're Christian. This is Principal Vagina. No relation.
-            </p>
-        </div>
-    );
+        Nothing you do matters, your existence is a lie! Burgertime! You know who's into dragons, Morty? Nerds who refuse to admit they're Christian. This is Principal Vagina. No relation.
+      </p>
+      <br />
+      <p>
+        Testing commit from nvim on Linux machine.
+      </p>
+    </div>
+  );
 }
